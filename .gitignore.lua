cf05baaf11286c44475c0e051a5c185ec4f22a6e
AGENTIR006 <148233435+AGENTIR006@users.noreply.github.com>
2023-11-26 18:15:31 +0300

    Update and rename .gitignore to .gitignore.lua

---
+++ .gitignore.lua
@@ -0,0 +1,5 @@
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
+
+local win = lib:Window("AGENT006",Color3.fromRGB(44, 120, 224), Enum.KeyCode.f)
+
+local tab = win:Tab("Script")
